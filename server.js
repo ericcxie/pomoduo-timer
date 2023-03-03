228a208437fbb69526cf6c21a4ae01016e09cbc0
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-03-03 00:56:31 -0500

    Create generate room code function

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const { Pool } = require("pg");
 
+const { generateRoomCode } = require("./utils/generateRoomCode");
+
 // Connect to the database
 const pool = new Pool({
   user: process.env.DB_USER,
@@ -15,9 +17,11 @@ const pool = new Pool({
 // Start the server
 const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}...`);
 });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+console.log(generateRoomCode(6));
