a71febea78dc7d0d07fbefb20a18c76e2ea84a07
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-03-03 00:42:58 -0500

    Setup pooling in postgreSQL with express

---
+++ server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const { Pool } = require("pg");
+
+// Connect to the database
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+// Start the server
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
